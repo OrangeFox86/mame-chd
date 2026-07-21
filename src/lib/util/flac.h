796e19ae0a5f20ac5c423a4931b8ca0b1610227b
Rikki Gibson <rikkigibson@gmail.com>
2026-07-21 15:06:52 -0700

    Remove unnecessary license changes

--- src/lib/util/flac.h
+++ src/lib/util/flac.h
@@ -1,6 +1,5 @@
 // license:BSD-3-Clause
 // copyright-holders:Aaron Giles
-// Portions Copyright 2026 The Hollycast Authors
 /***************************************************************************
 
     flac.h
